7a7bfa73799ecbe9d82a8de15fabf7ef7946bd05
Eileen0917 <j405307@gmail.com>
2017-08-08 00:13:20 +0800

    delete comment in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@
 class User < ApplicationRecord
   has_many :dislikes
   has_many :categories, through: :dislike
-  # accepts_nested_attributes_for :dislikes, reject_if: :all_blank, allow_destroy: true
 end
